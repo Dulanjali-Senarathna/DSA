eab2c1fe39d06c0aa2484003cb8c2d11c1a342d4
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-02-21 10:37:20 +0530

    Implement Iteration

--- DSA/Program.cs
+++ DSA/Program.cs
@@ -4,9 +4,20 @@ namespace DSA
 {
     class Program
     {
+        public void calculateIterative(int n)
+        {
+            while (n > 0)
+            {
+                int k = n * n;
+                Console.WriteLine(k);
+                n = n - 1;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program r = new Program();
+            r.calculateIterative(4);
+            Console.ReadKey();
         }
     }
 }
